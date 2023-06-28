029bd1829f151f3285e24c50b30892d32512c692
Brian Estrada <lovesguitar@gmail.com>
2023-06-28 21:09:13 +0200

    chore: first commit

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+val kotlinVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.8.22"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+}
+
+version = "1.0.0"
+group = "com.monta.otel"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    // Tracing
+    val otelVersion = "1.27.0"
+    implementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure:${otelVersion}-alpha")
+    implementation("io.opentelemetry:opentelemetry-semconv:${otelVersion}-alpha")
+    implementation("io.opentelemetry.contrib:opentelemetry-samplers:${otelVersion}-alpha")
+}
+
+sourceSets {
+    main {
+        java.srcDirs("src/main/kotlin")
+        resources.srcDirs("src/main/resources")
+    }
+    test {
+        java.srcDirs("src/test/kotlin")
+        resources.srcDirs("src/test/resources")
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+ktlint {
+    filter {
+        exclude("**/generated/**")
+        include("**/kotlin/**")
+    }
+}
+
+tasks {
+    shadowJar {
+        archiveBaseName.set("otel-extension")
+        archiveVersion.set("")
+        archiveClassifier.set("")
+    }
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    }
+    processTestResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    }
+}
