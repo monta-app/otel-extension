9b586c5b839096fdde78c6e13b7b9f43593749a2
Morten Andersen <ma@monta.app>
2025-04-11 11:16:35 +0200

    chore: Change location of `shadow` plugin
    * context [slack](https://monta-app01.slack.com/archives/C04JANZMTQV/p1744272024820779)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.10"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.6"
     id("org.jlleitschuh.gradle.ktlint") version "12.2.0"
 }
 
