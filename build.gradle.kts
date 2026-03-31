6feb068342b6eff9262710bfc053b1be7f47ace9
Markus Becker <mtib.becker@gmail.com>
2026-03-31 08:27:48 +0200

    chore(deps): upgrade shadow 8.3.10 and ktlint 13.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.3.20"
-    id("com.gradleup.shadow") version "8.3.8"
-    id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
+    id("com.gradleup.shadow") version "8.3.10"
+    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
 }
 
 version = "1.0.0"
